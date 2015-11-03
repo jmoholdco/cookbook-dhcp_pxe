b809f4705198b027dab97949f755613f5d422800
Morgan Lieberthal <morgan@jmorgan.org>
2015-11-02 21:46:16 -0700

    Fix attribute typo

--- attributes/tftp.rb
+++ attributes/tftp.rb
@@ -1,6 +1,6 @@
 default['tftp'] = {
   'server' => node['ipaddress'],
-  'filename' => 'pxelinux. 0',
+  'filename' => 'pxelinux.0',
   'syslinux' => {
     'files' => %w( pxelinux.0 menu.c32 memdisk mboot.c32 chain.c32 )
   },
